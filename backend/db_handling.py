57eae19ca8be641a0e1b563966043749f6736618
AdrianDajnowski99 <adrian.dajnowski99@gmail.com>
2025-03-14 17:54:03 +0100

    Patch (WIP)

--- backend/db_handling.py
+++ backend/db_handling.py
@@ -7,7 +7,7 @@ class DbHandling:
     @staticmethod
     def connect_to_db():
         conn = psycopg2.connect(
-            dbname="tasmanDb",
+            dbname="tasks_2rfh",
             user="adrian99",
             password="baHBJlnWjzhc7y0hcRyVi1xl4yKhKFnJ",
             host="dpg-cv85t4ogph6c739b0di0-a",
