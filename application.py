2ba6b74c9cdbd6e93095d57a20b422f9394cbae4
AdrianDajnowski99 <adrian.dajnowski99@gmail.com>
2025-07-09 18:42:42 +0200

    2.8.4.5

--- application.py
+++ application.py
@@ -64,7 +64,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'username' not in session:
-            return redirect(url_for('login'))
+            return redirect(url_for('home'))
         return f(*args, **kwargs)
     return decorated_function
 
--- frontend/static/js/auth.js
+++ frontend/static/js/auth.js
@@ -1,21 +1,24 @@
-//
 const loginBtn = document.getElementById("Login");
 const registerBtn = document.getElementById("Register");
 
-//
-loginBtn.addEventListener("click", () => {
-    const loginUrl = loginBtn.getAttribute("data-url");
-    window.location.href = loginUrl;
-});
+if (loginBtn) {
+    loginBtn.addEventListener("click", () => {
+        const loginUrl = loginBtn.getAttribute("data-url");
+        window.location.href = loginUrl;
+    });
+}
 
-registerBtn.addEventListener("click", () => {
-    const registerUrl = registerBtn.getAttribute("data-url");
-    window.location.href = registerUrl;
-});
+if (registerBtn) {
+    registerBtn.addEventListener("click", () => {
+        const registerUrl = registerBtn.getAttribute("data-url");
+        window.location.href = registerUrl;
+    });
+}
 
 const logoutBtn = document.getElementById('logout');
 if (logoutBtn) {
     logoutBtn.addEventListener('click', () => {
+        console.log('Logout clicked');
         window.location.href = '/logout';
     });
 }
